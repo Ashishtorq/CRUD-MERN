3292f1091001e47ccca262e6b9b6b4bbae95f0fc
Ashistorq <ashishsinghtarwa@gmail.com>
2024-01-11 13:00:38 +0530

    client

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { Fragment, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+    <Fragment>
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Fragment>
+  );
+}
+
+export default App;
