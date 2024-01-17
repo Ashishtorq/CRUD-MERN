f782f163cf7352dc05f183aa47df97ee0434c085
Ashish Singh <98528335+Ashishtorq@users.noreply.github.com>
2024-01-17 20:45:40 +0530

    Update App.jsx

--- Table Search implementation/frontend/src/App.jsx
+++ Table Search implementation/frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Fragment, useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([])
   const [search, setSearch] = useState("");
 
 
