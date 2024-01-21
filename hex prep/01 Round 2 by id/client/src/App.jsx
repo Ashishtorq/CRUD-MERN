5cccfa8a9e853bee1120dc9098d0c71cb6024b38
Ashistorq <ashishsinghtarwa@gmail.com>
2024-01-21 16:13:10 +0530

    round 2 by id

---
+++ hex prep/01 Round 2 by id/client/src/App.jsx
@@ -0,0 +1,76 @@
+import { Fragment, useCallback, useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState("")
+  const callApi = async () => {
+    const userInfo = await fetch("http://localhost:4000/user");
+    const userData = await userInfo.json();
+    console.log(userData);
+    setUsers(userData);
+  };
+  useEffect(() => {
+    callApi();
+  }, []);
+  const callById = useCallback(async(id)=>{
+    const userInfo = await fetch(`http://localhost:4000/user/${id}`);
+    const userData = await userInfo.json();
+    console.log(userData)
+    setUsers([userData])
+  },[])
+  useEffect(()=>{
+    const debounce = setTimeout(()=>{
+      if(search){
+        callById(search)
+      }else{
+        callApi()
+      }
+    },1000)
+   return ()=> clearTimeout(debounce)
+  },[search, callApi, callById])
+  return (
+    <Fragment>
+      <h1>Table</h1>
+      <div className="search-box">
+      <input type="text" placeholder="Search By Id" 
+      onChange={(e)=>{
+        setSearch(e.target.value)
+      }}
+      />
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Username</td>
+            <td>Email</td>
+            <td>Address</td>
+            <td>Phone</td>
+            <td>Website</td>
+            <td>Company</td>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => {
+            return (
+              <tr>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>{user.address}</td>
+                <td>{user.phone}</td>
+                <td>{user.website}</td>
+                <td>{user.company}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ hex prep/01 Round 2 by id/server/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const PORT = 4000;
+const app = express();
+
+app.use(express());
+app.use(cors());
+
+app.get("/user", async (req, res) => {
+  const userInfo = await axios("https://jsonplaceholder.typicode.com/users");
+  const userData = userInfo.data;
+
+  let respose = [];
+  userData.forEach((user) => {
+    let userObj = {};
+    userObj.id = user.id;
+    userObj.name = user.name;
+    userObj.username = user.username;
+    userObj.email = user.email;
+    userObj.phone = user.phone;
+    userObj.website = user.website;
+    userObj.address = `${user.address.city}, ${user.address.street}`;
+    userObj.company = `${user.company.name},${user.company.bs} `;
+
+    respose.push(userObj)
+  });
+  res.send(respose)
+});
+
+app.get("/user/:id",async(req,res)=>{
+  const userInfo = await axios("http://localhost:4000/user");
+  const userData = userInfo.data;
+  const userid = parseInt(req.params.id)
+  const userFeild = userData.find(user => user.id === userid)
+  if(userFeild){
+    res.send(userFeild).status(201)
+  }else{
+    res.send({message:"NO Data Found"}).status(404)
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
